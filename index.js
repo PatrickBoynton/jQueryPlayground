ac1593e2b29af1b2fbf799ab99ad18bde4972748
Patrick Boynton <jpboynton3@gmail.com>
2021-10-18 10:35:17 -0400

    Removed the edit methods, cleaned up code a bit.

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 // Helper method for creating the UI.
 const addCard = (todo) => {
   const div = $(".items");
-  let todoTitle = todo.title;
 
   let todoCard =
     '<div class="card card-body m-5"><h5 class="card-title" id="title">' +
-    todoTitle +
+    todo +
     '</h5> <div class="d-flex"><a href="#" class="card-link edit-link fw-bold" id="edit">Edit</a>' +
     '<a href="#" class="card-link fw-bold delete-link" id="delete">Delete</a></div> </div>';
 
@@ -13,10 +12,11 @@ const addCard = (todo) => {
 };
 
 // Create
-$("#add").on("click", () => {
+$("form").on("submit", (e) => {
+  e.preventDefault();
   let todos = JSON.parse(localStorage.getItem("todos"));
 
-  let todo = {id: Math.random() * 1000, title: $("#add-todo").val() };
+  let todo = $("#add-todo").val();
 
   if (todos === null) todos = [];
 
@@ -24,7 +24,7 @@ $("#add").on("click", () => {
 
   localStorage.setItem("todos", JSON.stringify(todos));
 
-  addCard(storage);
+  addCard(todo);
 });
 
 // Read
@@ -34,47 +34,10 @@ $(window).on("load", () => {
   storage?.map((t) => addCard(t));
 });
 
-// Change to edit mode.
-$(document).on("click", "#edit", (e) => {
-  const title = $("#title").text();
-  const input = $("#add-todo");
-  const button = $("button");
-
-  input.val(title);
-
-  button.attr("id", "edit-todo");
-
-  button.text("Edit Todo");
-});
-
-// Edit the todo.
-$(document).on("click", "#edit-todo", (e) => {
-  e.preventDefault();
-
-  const inputTitle = $("input").val();
-  const title = $("#title").text();
-
-  if (inputTitle === title) {
-    const todo = localStorage.setItem(
-      "todos",
-      JSON.stringify({ title: inputTitle })
-    );
-
-    addCard(todo);
-    location.reload;
-  }
-});
-
 // Delete
 $(document).on("click", "#delete", (e) => {
-  const h5 = $("#title");
-  const div = $(".items");
+  const title = $("#title").text();
+  const todos = JSON.parse(localStorage.getItem("todos"));
 
-  let index = JSON.parse(localStorage.getItem("todos")).findIndex(
-    (x) => x.title === h5.text()
-  );
-
-  localStorage.removeItem("todos", index);
-
-  div.remove();
+  console.log(todos.indexOf(title));
 });
