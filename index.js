f19568f304fa83dcbdc0b03f8555687792872707
Patrick Boynton <jpboynton3@gmail.com>
2021-10-19 09:19:37 -0400

    Added some functionality to edit mode.

--- index.js
+++ index.js
@@ -5,15 +5,14 @@ const addCard = (todo) => {
   let todoCard =
     '<div class="card card-body m-5"><h5 class="card-title">' +
     todo +
-    '</h5> <div class="d-flex"><a href="#" class="card-link edit-link fw-bold">Edit</a>' +
+    '</h5> <div class="d-flex"><a href="#" class="card-link edit-link fw-bold edit">Edit</a>' +
     '<a href="#" class="card-link fw-bold delete-link delete">Delete</a></div> </div>';
 
   div.append(todoCard);
 };
 
-// Create
-$("form").on("submit", (e) => {
-  e.preventDefault();
+// Create or edit depending on button id
+$("form").on("submit", () => {
   let todos = JSON.parse(localStorage.getItem("todos"));
 
   let todo = $("#add-todo").val();
@@ -32,6 +31,15 @@ $(window).on("load", () => {
   const storage = JSON.parse(localStorage.getItem("todos"));
 
   storage?.map((t) => addCard(t));
+});
+
+// Set Edit Mode
+$(document).on("click", ".edit", function (e) {
+  const storage = JSON.parse(localStorage.getItem("todos"));
+  const card = $(this).closest(".card");
+  const title = card.children("h5").text();
+  $("#add-todo").val(title);
+  const button = $("#add").text("Edit Todo").attr("id", "edit");
 });
 
 // Delete
